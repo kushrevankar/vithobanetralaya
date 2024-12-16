c498e4813d1a6c04d41bb44bd61863318122b321
kush <kushrevankar24@gmail.com>
2024-12-16 14:56:57 +0530

    Footer Created

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Herobel from './components/herobel/Herobel';
 import Timings from './components/timings/Timings';
 import Diseases from './components/diseases/Diseases';
 import Abovefooter from './components/abovefooter/Abovefooter';
+import Footer from './components/footer/Footer'
 import { BrowserRouter } from 'react-router-dom';
 
 const App = () => {
@@ -18,6 +19,7 @@ const App = () => {
         <Timings />
         <Diseases />
         <Abovefooter />
+        <Footer />
       </div>
     </BrowserRouter>
   );
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from 'react';
+import { MapPin, Phone, Mail } from 'lucide-react';
+import styles from './footer.module.css';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div className={styles.contact}>
+          <h3>Contact Us</h3>
+          <div className={styles.item}>
+            <MapPin />
+            <p>To be filled</p>
+          </div>
+          <div className={styles.item}>
+            <Phone />
+            <p>To be filled</p>
+          </div>
+          <div className={styles.item}>
+            <Mail />
+            <p>To be filled</p>
+          </div>
+        </div>
+        
+        <div className={styles.links}>
+          <h3>Quick Links</h3>
+          <ul>
+            <li><a href="/about">About Us</a></li>
+            <li><a href="/doctors">Our Doctors</a></li>
+            <li><a href="/facilities">Facilities</a></li>
+            <li><a href="/treatments">Treatments</a></li>
+          </ul>
+        </div>
+        
+        <div className={styles.emergency}>
+          <h3>Emergency</h3>
+          <p>24/7 Emergency Services Available</p>
+          <div className={styles.hotline}>
+            Emergency Hotline: (555) 911-2345
+          </div>
+        </div>
+      </div>
+      
+      <div className={styles.copyright}>
+        <p>&copy; 2024 Vithoba Netralaya Eye Hospital. All Rights Reserved.</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
