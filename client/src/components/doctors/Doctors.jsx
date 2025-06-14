6e0667c284dfe0e7afa50cbe3fab3098987bbc5d
kush <kushrevankar24@gmail.com>
2025-06-15 01:24:14 +0530

    treatments section done

--- client/src/components/doctors/Doctors.jsx
+++ client/src/components/doctors/Doctors.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import styles from './doctors.module.css'
+import doctorsBg from '../../images/doctorsng.jpg'
+//import doctor from '../../images/anant.jpg'
 
 const Doctors = () => {
   return (
-    <div>Doctors</div>
+    <div className={styles.principle}>
+          <div
+            className={styles.background}
+            style={{ backgroundImage: `url(${doctorsBg})` }}
+          >
+            <div className={styles.container}>
+              <div className={styles.heading}>Doctors</div>
+            </div>
+          </div>
+          <div className={styles.descrips}>
+            <div className={styles.description}>
+              {/*<img src={doctor} alt="dr. anant revankar" className={styles.floatedImage} />*/}
+              <h1 style={{ color: "black" }}>Our Doctors</h1>
+              <p>
+                Dr. Anant Revankar is the driving force behind Vithoba Netralaya Eye Clinic—a skilled and deeply compassionate ophthalmologist dedicated to delivering top-tier eye care at fair and accessible prices. 
+                Available year-round, Dr. Revankar personally guides every patient through the treatment process, offering everything from LASIK and micro-incision cataract surgery to oculoplastic procedures and pediatric 
+                eye care—all within a single-doctor setup. He chose this model intentionally: instead of outreach camps, he brings outreach into his clinic, keeping quality care within reach for the economically disadvantaged.
+                With empathy as his guiding principle, Dr. Revankar prides himself on honest, ethical, and reliable service—treating patients with respect, clear communication, and unwavering professionalism. 
+                This commitment doesn’t just show in his surgical skill but in the trust and gratitude reflected by those he treats. Though trained in ophthalmology, his true specialty is care—with every person 
+                who walks through the clinic getting the same level of compassion and expertise you’d expect from a much larger team
+              </p>
+            </div>
+          </div>
+          <div className={styles.reviews}>
+            <div className={styles.text}>
+              <h1 style={{ color: "black", paddingBottom: 15 }}>
+                Testimonials
+              </h1>
+              <p style={{ color: "#555", fontSize: 30 }}>
+                "The eye specialist Dr. Anant highly skilled and kind.. He gently
+                listened to my eye condition that has been troubling me for a while.
+                I am of the opinion his core strength is his skill and empathy."
+              </p>
+              <p style={{ color: "#555", textAlign: "right", fontSize: 22 }}>
+                — Aafaq, JustDial
+              </p>
+            </div>
+          </div>
+        </div>
   )
 }
 
--- client/src/components/treatments/Treatments.jsx
+++ client/src/components/treatments/Treatments.jsx
@@ -1,9 +1,155 @@
-import React from 'react'
+import React from "react";
+import styles from "./treatments.module.css";
+import medicBg from "../../images/medicines.jpg";
+import Card from "@mui/joy/Card";
+import AspectRatio from "@mui/joy/AspectRatio";
+import CardContent from "@mui/joy/CardContent";
+import CardOverflow from "@mui/joy/CardOverflow";
+import Typography from "@mui/joy/Typography";
+import eyeExam from "../../images/eyeexam.jpg";
+import lasik from "../../images/lasik.jpg";
+import cataract from "../../images/cataract.webp";
+import refractive from "../../images/refractive.jpg";
+import dryEye from "../../images/dryeye.webp";
+import stye from "../../images/stye.jpg";
+import csr from "../../images/csr.jpg";
+import glaucoma from "../../images/glaucoma.jpg";
+import macular from "../../images/macular.jpg";
 
 const Treatments = () => {
   return (
-    <div>Treatments</div>
-  )
-}
+    <div className={styles.principle}>
+      <div
+        className={styles.background}
+        style={{ backgroundImage: `url(${medicBg})` }}
+      >
+        <div className={styles.container}>
+          <div className={styles.heading}>Treatments</div>
+        </div>
+      </div>
+      <div className={styles.descrips}>
+        <div className={styles.description}>
+          <h1 style={{ color: "black" }}>Treatments</h1>
+          <p>
+            The clinic provides comprehensive ophthalmic care, ranging from
+            routine eye examinations to advanced surgical interventions. A full
+            spectrum of diagnostic and therapeutic services is offered, ensuring
+            that each patient receives tailored treatment based on their
+            specific condition. The facility is also equipped to perform
+            cutting-edge laser vision correction procedures, including LASIK,
+            Intralase, ReLEx SMILE, and Contoura Vision, all designed to enhance
+            visual accuracy and patient comfort. With a strong emphasis on
+            clinical precision, modern technology, and patient-centered care,
+            the clinic maintains the highest standards of excellence in every
+            aspect of service delivery.
+          </p>
+        </div>
+      </div>
+      <div className={styles.cards}>
+        <div className={styles.space}>
+          <Card variant="soft" sx={{ width: 360 }}>
+            <CardOverflow>
+              <AspectRatio ratio="2">
+                <img src={eyeExam} alt="Eye exam" loading="lazy" />;
+              </AspectRatio>
+            </CardOverflow>
+            <CardContent>
+              <Typography level="title-md" sx={{ fontSize: '1.3rem' }}>Comprehensive Eye Exams</Typography>
+              <Typography level="body-sm" sx={{ fontSize: '1.0rem' }}>A thorough evaluation of your vision and eye health, aimed at detecting refractive errors, eye diseases, and early signs of systemic conditions affecting the eyes.</Typography>
+            </CardContent>
+          </Card>
+          <Card variant="soft" sx={{ width: 360 }}>
+            <CardOverflow>
+              <AspectRatio ratio="2">
+                <img src={lasik} alt="Lasik" loading="lazy" />;
+              </AspectRatio>
+            </CardOverflow>
+            <CardContent>
+              <Typography level="title-md" sx={{ fontSize: '1.3rem' }}>LASIK & Laser Vision Correction</Typography>
+              <Typography level="body-sm" sx={{ fontSize: '1.0rem' }}>Safe, precise laser procedures that reshape the cornea to correct refractive errors—helping reduce or eliminate the need for glasses or contact lenses.</Typography>
+            </CardContent>
+          </Card>
+          <Card variant="soft" sx={{ width: 360 }}>
+            <CardOverflow>
+              <AspectRatio ratio="2">
+                <img src={cataract} alt="Cataract" loading="lazy" />;
+              </AspectRatio>
+            </CardOverflow>
+            <CardContent>
+              <Typography level="title-md" sx={{ fontSize: '1.3rem' }}>Cataract Surgery</Typography>
+              <Typography level="body-sm" sx={{ fontSize: '1.0rem' }}>A quick and effective procedure to remove the clouded natural lens of the eye and replace it with a clear artificial one, restoring sharp vision.</Typography>
+            </CardContent>
+          </Card>
+          <Card variant="soft" sx={{ width: 360 }}>
+            <CardOverflow>
+              <AspectRatio ratio="2">
+                <img src={stye} alt="stye" loading="lazy" />;
+              </AspectRatio>
+            </CardOverflow>
+            <CardContent>
+              <Typography level="title-md" sx={{ fontSize: '1.3rem' }}>Stye Treatment</Typography>
+              <Typography level="body-sm" sx={{ fontSize: '1.0rem' }}>Targeted care for red, painful eyelid bumps—using warm compresses, hygiene, or minor procedures to ease discomfort and speed healing.</Typography>
+            </CardContent>
+          </Card>
+          <Card variant="soft" sx={{ width: 360 }}>
+            <CardOverflow>
+              <AspectRatio ratio="2">
+                <img src={refractive} alt="Refractive Error Treatments" loading="lazy" />;
+              </AspectRatio>
+            </CardOverflow>
+            <CardContent>
+              <Typography level="title-md" sx={{ fontSize: '1.3rem' }}>Refractive Error Treatments</Typography>
+              <Typography level="body-sm" sx={{ fontSize: '1.0rem' }}>Correction of vision problems like nearsightedness, farsightedness, and astigmatism using glasses, contact lenses, or refractive surgery.</Typography>
+            </CardContent>
+            </Card>
+            <Card variant="soft" sx={{ width: 360 }}>
+            <CardOverflow>
+              <AspectRatio ratio="2">
+                <img src={dryEye} alt="Dry Eye" loading="lazy" />;
+              </AspectRatio>
+            </CardOverflow>
+            <CardContent>
+              <Typography level="title-md" sx={{ fontSize: '1.3rem' }}>Dry Eye Treatment</Typography>
+              <Typography level="body-sm" sx={{ fontSize: '1.0rem' }}>Relief for irritated, dry eyes through lubricating drops, medications, or procedures that improve tear production and retention.</Typography>
+            </CardContent>
+          </Card>
+          <Card variant="soft" sx={{ width: 360 }}>
+            <CardOverflow>
+              <AspectRatio ratio="2">
+                <img src={csr} alt="csr" loading="lazy" />;
+              </AspectRatio>
+            </CardOverflow>
+            <CardContent>
+              <Typography level="title-md" sx={{ fontSize: '1.3rem' }}>Central Serous Retinopathy (CSR)</Typography>
+              <Typography level="body-sm" sx={{ fontSize: '1.0rem' }}>A condition where fluid builds up under the retina, affecting vision—managed with lifestyle changes, observation, or laser treatment as needed.</Typography>
+            </CardContent>
+          </Card>
+          <Card variant="soft" sx={{ width: 360 }}>
+            <CardOverflow>
+              <AspectRatio ratio="2">
+                <img src={glaucoma} alt="glaucoma" loading="lazy" />;
+              </AspectRatio>
+            </CardOverflow>
+            <CardContent>
+              <Typography level="title-md" sx={{ fontSize: '1.3rem' }}>Glaucoma</Typography>
+              <Typography level="body-sm" sx={{ fontSize: '1.0rem' }}>A group of eye conditions that damage the optic nerve, often linked to high eye pressure. Early diagnosis and treatment help prevent vision loss.</Typography>
+            </CardContent>
+          </Card>
+          <Card variant="soft" sx={{ width: 360 }}>
+            <CardOverflow>
+              <AspectRatio ratio="2">
+                <img src={macular} alt="macular" loading="lazy" />;
+              </AspectRatio>
+            </CardOverflow>
+            <CardContent>
+              <Typography level="title-md" sx={{ fontSize: '1.3rem' }}>Macular Degeneration</Typography>
+              <Typography level="body-sm" sx={{ fontSize: '1.0rem' }}>A progressive condition affecting the central retina, leading to blurred or distorted vision. Early detection helps slow vision loss and preserve quality of life.</Typography>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Treatments
+export default Treatments;
